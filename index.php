d28d22e2b4f11cac18cb563c8252811f4d8037c3
DESKTOP-EVT7QHE\yu_fy <yusril.fikry1998@gmail.com>
2018-03-14 15:31:27 +0700

    upload test

---
+++ index.php
@@ -0,0 +1,9 @@
+<h4>Daftar Sekolah Di Malang</h4>
+    <form action="action.php" method="GET">
+    <select name="tingkat">
+    <option value="SMP">SMP</option>
+    <option value="SMA">SMA</option>
+    <option value="SMK">SMK</option>
+    </select>
+    <input type="submit" name="tampil" value="Tampilkan">
+    </form>
